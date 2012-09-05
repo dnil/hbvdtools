af8acc19f7ca193dd34c149c1cd1314d24102177
jessada <jessada@kth>
2012-09-05 17:22:37 +0200

    remove testcase from repo

--- testcase/bvd_get/case_bvd_get_02/create_expected_result.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-grep "^8" $1 | awk '{print "10\t"$1"\t"$1"\tG\tA\t"$2;}'
--- testcase/normalize_vcf/unit_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-$scriptdir/case_normalize_vcf_01/validate_result.sh
-
-
-
